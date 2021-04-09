8364680a1d98e3cf42168209b218eedce4f7b70b
olhaknysh <olhaknysh1994@gmail.com>
2021-04-10 00:30:27 +0300

    Updates

--- static/js/main.11cdbd9a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{9:function(t,e,o){"use strict";o.r(e);var n=o(1),c=o.n(n),r=o(3),i=o.n(r),s=o(0),h=function(){return Object(s.jsx)("div",{})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(h,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.11cdbd9a.chunk.js.map
